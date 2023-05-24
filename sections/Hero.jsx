2c889788c3af7f5d05f356764ecebe80d251ea6e
Muhammad-Saad0 <119536957+Muhammad-Saad0@users.noreply.github.com>
2023-05-24 19:16:13 +0500

    hero section completed(no animations)

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -4,7 +4,7 @@ import styles from "../styles";
 const Hero = () => {
   return (
     <section className="relative">
-      <div className="flex flex-col items-center inset-0 gap-3 z-10 relative">
+      <div className="flex flex-col items-center z-10 relative">
         <h1 className="text-white font-extrabold text-[100px]">
           METAVERSE
         </h1>
@@ -18,13 +18,23 @@ const Hero = () => {
           </h1>
         </div>
       </div>
-      <div className="relative ml-20">
-        <div className="absolute z-[0] -top-[50px] h-[300px] w-full">
+      <div className="relative ml-[100px] h-[350px]">
+        <div className="absolute z-[0] -top-[50px] h-full w-full">
           <img
             src="/cover.png"
-            className="rounded-tl-[140px] h-[350px] w-full object-cover relative rounded-bl-[70px]"
-            alt=""
+            className="rounded-tl-[100px] h-[350px] w-full object-cover relative rounded-bl-[30px]"
+            alt="cover"
           />
+          <div
+            className="absolute bottom-0 right-[70px] w-[115px]
+           transform translate-y-1/2"
+          >
+            <img
+              src="/stamp.png"
+              className="h-full w-full object-contain"
+              alt="stamp"
+            />
+          </div>
         </div>
       </div>
     </section>
