2420f38a1ce28265ef6153b630ceaa60622e07fc
Muhammad-Saad0 <119536957+Muhammad-Saad0@users.noreply.github.com>
2023-05-27 12:38:15 +0500

    explore section completed

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -1,18 +1,48 @@
 "use client";
 
-const ExploreCard = ({ id, image, title }) => (
-  <div className="h-[600px] w-[200px] rounded-3xl overflow-hidden relative">
-    <img
-      className="h-full w-full object-cover absolute z-0"
-      src={image}
-      alt="world-image"
-    />
-    <div className="flex flex-col justify-end h-full z-10">
-      <h4 className="text-white font-semibold mb-[100px] text-[26px] -rotate-90">
-        {title}
-      </h4>
+const ExploreCard = ({
+  id,
+  image,
+  title,
+  isActive,
+  setIsActive,
+}) => {
+  const handleClick = (event) => {
+    event.preventDefault();
+    console.log(id);
+    setIsActive(id);
+  };
+  return (
+    <div
+      onClick={handleClick}
+      className={`h-[600px] rounded-3xl overflow-hidden relative`}
+    >
+      <img
+        className="h-full w-full object-cover absolute z-0"
+        src={image}
+        alt="world-image"
+      />
+      <div className="flex flex-col justify-end h-full relative z-10">
+        {isActive === id && (
+          <div className="is-selected-background absolute bottom-0 h-1/3 w-full" />
+        )}
+        {isActive === id && (
+          <p className="text-center relative z-10 text-white mb-4 cursor-pointer">
+            Enter Metaverse
+          </p>
+        )}
+        <h4
+          className={`text-white raltive z-10 text-left font-semibold text-[20px] ${
+            isActive === id
+              ? "text-center text-[28px] mb-4"
+              : "-rotate-90 mb-[100px]"
+          }`}
+        >
+          {title}
+        </h4>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default ExploreCard;
--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -4,6 +4,7 @@ import TypingText from "../components/TypingText";
 import { motion } from "framer-motion";
 import { exploreWorlds } from "../constants";
 import ExploreCard from "../components/ExploreCard";
+import { useEffect, useState } from "react";
 
 const staggerContainerVariants = {
   hidden: {},
@@ -23,48 +24,65 @@ const exploreCardVariants = {
   },
 };
 
-const Explore = () => (
-  <section className="relative">
-    <div
-      className="cyan-gradient absolute
+const Explore = () => {
+  useEffect(() => {
+    setIsActive(exploreWorlds[0].id);
+  }, []);
+
+  const [isActive, setIsActive] = useState("");
+  return (
+    <section className="relative">
+      <div
+        className="cyan-gradient absolute
     h-1/3 w-1/2 top-0 -left-1/2 z-[0]"
-    />
-    <div
-      className="blue-gradient absolute
+      />
+      <div
+        className="blue-gradient absolute
     h-1/3 w-2/3 -bottom-1/4 -left-1/2 z-[0]"
-    />
-    <div
-      className="purple-gradient absolute
+      />
+      <div
+        className="purple-gradient absolute
     h-1/3 w-1/2 top-1/2 -left-1/2 z-[0]"
-    />
-    <TypingText
-      title={"| The World"}
-      styles={"text-center"}
-    />
-    <h3 className="font-bold text-6xl text-center text-white my-6">
-      Choose the world you want <br /> to explore
-    </h3>
-    <motion.div
-      variants={staggerContainerVariants}
-      whileInView={"show"}
-      initial={"hidden"}
-      className="flex flex-row justify-between p-12 mx-10"
-    >
-      {exploreWorlds.map((world) => {
-        return (
-          <motion.div
-            variants={exploreCardVariants}
-          >
-            <ExploreCard
-              id={world.id}
-              title={world.title}
-              image={world.imgUrl}
-            />
-          </motion.div>
-        );
-      })}
-    </motion.div>
-  </section>
-);
+      />
+      <TypingText
+        title={"| The World"}
+        styles={"text-center"}
+      />
+      <h3 className="font-bold text-6xl text-center text-white my-6">
+        Choose the world you want <br /> to
+        explore
+      </h3>
+      <motion.div
+        variants={staggerContainerVariants}
+        whileInView={"show"}
+        initial={"hidden"}
+        viewport={{ once: false, amount: 0.3 }}
+        className="flex flex-row justify-between gap-6 p-12 mx-10"
+      >
+        {exploreWorlds.map((world, index) => {
+          return (
+            <motion.div
+              key={index}
+              className={`${
+                isActive === world.id
+                  ? "w-2/6"
+                  : "w-1/6"
+              }`}
+              variants={exploreCardVariants}
+            >
+              <ExploreCard
+                id={world.id}
+                title={world.title}
+                image={world.imgUrl}
+                isActive={isActive}
+                setIsActive={setIsActive}
+              />
+            </motion.div>
+          );
+        })}
+      </motion.div>
+    </section>
+  );
+};
 
 export default Explore;
